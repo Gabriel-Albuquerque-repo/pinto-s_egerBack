e1bccbf79070788d75cea2f0ce8bbf617a3bbb84
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2021-11-19 20:41:01 -0300

    [ADD] arq server; separando do app

---
+++ src/app.ts
@@ -0,0 +1,55 @@
+import 'dotenv/config';
+// import http from 'http';
+import express from 'express';
+import passport from 'passport';
+import cookieSession from 'cookie-session';
+import './passportSetup.ts';
+
+const app = express();
+// app.use(express.static(`${__dirname}/../public`));
+
+// const serverHttp = http.createServer(app);
+
+app.use(cookieSession({
+  name: 'web session',
+  keys: ['key1', 'key2'],
+}));
+
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// app.get('/', (_, response) => response.sendFile('index.html'));
+
+app.get('/failed', (_, response) => response.send('You failed to log in!'));
+
+app.get('/good', isLoggedIn, (request, response) => response.send(`Welcome ${request.user.displayName}!`));
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }),
+);
+
+app.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/failed', successRedirect: '/good' }),
+  (_, response) => {
+    // Successful authentication, redirect home.
+    response.redirect('/good');
+  },
+);
+
+app.get('/logout', (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect('/');
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,52 +1,8 @@
-import 'dotenv/config';
-import express from 'express';
-import passport from 'passport';
-import cookieSession from 'cookie-session';
-import './passportSetup.ts';
+/* eslint-disable import/extensions */
+// eslint-disable-next-line import/no-unresolved
+import app from './app';
 
-const app = express();
-
-// app.use(express.static(`${__dirname}/../public`));
-
-app.use(cookieSession({
-  name: 'web session',
-  keys: ['key1', 'key2'],
-}));
-
-const isLoggedIn = (req, res, next) => {
-  if (req.user) {
-    next();
-  } else {
-    res.sendStatus(401);
-  }
-};
-app.use(passport.initialize());
-app.use(passport.session());
-
-// app.get('/', (_, response) => response.sendFile('index.html'));
-
-app.get('/failed', (_, response) => response.send('You failed to log in!'));
-
-app.get('/good', isLoggedIn, (request, response) => response.send(`Welcome ${request.user.displayName}!`));
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] }),
+app.listen(
+  process.env.PORT_SERVER,
+  () => console.log('Server running on PORT:', process.env.PORT_SERVER),
 );
-
-app.get(
-  '/google/callback',
-  passport.authenticate('google', { failureRedirect: '/failed', successRedirect: '/good' }),
-  (_, response) => {
-    // Successful authentication, redirect home.
-    response.redirect('/good');
-  },
-);
-
-app.get('/logout', (req, res) => {
-  req.session = null;
-  req.logout();
-  res.redirect('/');
-});
-
-app.listen(8000);
