eec098b5e0c09c0d5956d22285113512055c9cd1
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2021-11-23 20:23:31 -0300

    [UPD] initial commit

--- src/useCases/createUser/CreateUserController.ts
+++ src/useCases/createUser/CreateUserController.ts
@@ -1,3 +1,32 @@
-export default class CreateUserController {
+/* eslint-disable import/extensions */
+import { Request, Response } from 'express';
+// eslint-disable-next-line import/no-unresolved
+import CreateUserUseCase from './CreateUserUseCase';
 
+export default class CreateUserController {
+  private createUserUseCase: CreateUserUseCase;
+
+  constructor(createUserUseCase: CreateUserUseCase) {
+    this.createUserUseCase = createUserUseCase;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { googleId, name, email } = request.body;
+
+    try {
+      await this.createUserUseCase.execute({
+        googleId,
+        name,
+        email,
+        createdAt: Date.now(),
+      });
+
+      return response.status(201).send();
+    } catch (err) {
+      return response.status(400).json({
+        message: err.message || 'Unexpected error.',
+      });
+    }
+  }
 }
