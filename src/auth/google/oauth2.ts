ac0fba9318d8654049f9cdb5c14e57ba312c6784
Gabriel de Albuquerque <gabrieleac2009@gmail.com>
2021-11-24 11:18:30 -0300

    [UPD] refactor googleStrategy import

---
+++ src/auth/google/oauth2.ts
@@ -0,0 +1,36 @@
+import passport from 'passport';
+import googlePass from 'passport-google-oauth20';
+
+const GoogleStrategy = googlePass.Strategy;
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  },
+  // Aqui tem o acesso ao profile e faz a integração com o bd para procurar
+  // o usuário! esse é o cb
+  ((accessToken, refreshToken, profile, done) => {
+    // console.log(profile);
+    done(null, profile);
+  }),
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+// Procura no db se o ide existe
+// passport.serializeUser(async (id, cb) => {
+//   const user = await User.findOne({ where: { id } }).catch((err) => {
+//     console.log('Error deserializing', err);
+//     cb(err, null);
+
+//     if (user) cb(null, user);
+//   });
+// });
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
